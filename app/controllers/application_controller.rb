e2e8b59a20bf9720fbe611f9dda967c568dc16b3
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-10-13 15:27:19 +0200

    added tracker & custom erros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,13 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up,
+      keys: [:email, :username, :password, :password_confirmation])
 
+    devise_parameter_sanitizer.permit(:sign_in,
+      keys: [:login, :password, :password_confirmation])
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update,
+      keys: [:email, :username, :password_confirmation, :current_password])
   end
 end
