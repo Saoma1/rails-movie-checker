65cecf8030feacddf9c779a3a78e360b07171896
Sotirios Dimitriu <sotirios.dimitriou@gmail.com>
2021-10-13 13:35:48 +0200

    custom login done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
-class PagesController < ApplicationController
-  def home
+class MoviesController < ApplicationController
+  def index
     @movies = Movie.where(user_id: current_user.id)
   end
 end
